4b6c17882774465342d4067a92b34ec883f0acd3
CrazyDeveloperTG <119148554+CrazyDeveloperTG@users.noreply.github.com>
2022-12-19 09:44:33 +0530

    Update url_shortner.py

--- plugins/helper/url_shortner.py
+++ plugins/helper/url_shortner.py
@@ -1,4 +1,4 @@
-import o
+import os
 import aiohttp
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
